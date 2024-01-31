cd6800610b61f2a9a1c5c5aa9d4e4e4ae2fa109d
Ganesh Narwade <ganeshdnarwade7@gmail.com>
2024-01-31 10:06:54 +0530

    Services section added.

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+  
+  const openPopupButtons = document.querySelectorAll('.open-popup');
+  const popups = document.querySelectorAll('.popup');
+  const closePopupButtons = document.querySelectorAll('.close-popup');
+  
+  openPopupButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+      popups.forEach((popup) => {
+        popup.style.display = 'none';
+      });
+      popups[index].style.display = 'block';
+    });
+  });
+  
+  closePopupButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      popups.forEach((popup) => {
+        popup.style.display = 'none';
+      });
+    });
+  });
