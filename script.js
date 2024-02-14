678aad81a94b968486a736805416d95dc43c46ee
Ganesh Narwade <ganeshdnarwade7@gmail.com>
2024-02-14 19:40:41 +0530

    Website is now fully responsive

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
-  
-  const openPopupButtons = document.querySelectorAll('.open-popup');
-  const popups = document.querySelectorAll('.popup');
-  const closePopupButtons = document.querySelectorAll('.close-popup');
-  
-  openPopupButtons.forEach((button, index) => {
-    button.addEventListener('click', () => {
-      popups.forEach((popup) => {
-        popup.style.display = 'none';
-      });
-      popups[index].style.display = 'block';
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+const openPopupButtons = document.querySelectorAll('.open-popup');
+const popups = document.querySelectorAll('.popup');
+const closePopupButtons = document.querySelectorAll('.close-popup');
+
+openPopupButtons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    popups.forEach((popup) => {
+      popup.style.display = 'none';
+    });
+    popups[index].style.display = 'block';
+  });
+});
+
+closePopupButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    popups.forEach((popup) => {
+      popup.style.display = 'none';
     });
   });
-  
-  closePopupButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-      popups.forEach((popup) => {
-        popup.style.display = 'none';
-      });
-    });
-  });
+});
